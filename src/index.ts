bf0a871843fb53cc55114d48d0c57fd80155425a
Mikael Escolin <mikaelescolin@gmail.com>
2022-02-21 15:15:14 +0000

    Add table of country data
    - Added processing of json
    - Added processing of json data on client
    - Added table creation of data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
-import fs from 'fs';
+import fs from 'fs/promises';
 import url from 'url';
 import path from 'path';
+
+interface Countries {
+  [country: string]: string[];
+  continent: string[];
+  currency_code: string[];
+  city: string[];
+  tld: string[];
+  flag_base64: string[];
+}
+
+const countriesData: Countries = {
+  country: [],
+  continent: [],
+  currency_code: [],
+  city: [],
+  tld: [],
+  flag_base64: [],
+};
 
 function handleError(err: string, res: ServerResponse) {
   res.write(err);
@@ -9,21 +27,83 @@ function handleError(err: string, res: ServerResponse) {
 }
 
 async function sendFile(filePath: string, res: ServerResponse) {
-  fs.readFile(
-    path.join(
-      __dirname,
-      `public${filePath === '/' ? '/index.html' : filePath}`
-    ),
-    (err, data) => {
-      if (err) {
-        handleError(err.message, res);
-        return;
-      }
+  try {
+    const file = await fs.readFile(
+      path.join(
+        __dirname,
+        `public${filePath === '/' ? '/index.html' : filePath}`
+      )
+    );
 
-      res.write(data);
-      res.end();
+    if (filePath.includes('css')) {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
     }
-  );
+
+    res.write(file);
+    res.end();
+  } catch (err) {
+    handleError(err as string, res);
+  }
+}
+
+async function sendCountriesData(res: ServerResponse) {
+  if (countriesData.continent.length > 0) {
+    res.write(JSON.stringify(countriesData));
+    res.end();
+  } else {
+    const names = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-name.json')
+    );
+
+    const currencies = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-currency-code.json')
+    );
+
+    const capitals = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-capital-city.json')
+    );
+
+    const domain = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-domain-tld.json')
+    );
+
+    const flag = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-flag.json')
+    );
+
+    const continents = await fs.readFile(
+      path.join(__dirname, 'country-objects/country-by-continent.json')
+    );
+
+    const rawData = await Promise.all([
+      names,
+      continents,
+      currencies,
+      capitals,
+      domain,
+      flag,
+    ]);
+
+    const parsedData = rawData.map((currData) => JSON.parse(String(currData)));
+
+    Object.keys(countriesData).forEach((key, index) => {
+      countriesData[key] = parsedData[index].map((obj: any) => obj[key]);
+    });
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(countriesData));
+    res.end();
+  }
+}
+
+async function sendResource(resourcePath: string, res: ServerResponse) {
+  switch (resourcePath) {
+    case '/api/countries':
+      sendCountriesData(res);
+      break;
+    default:
+      handleError(`cannot find data for ${resourcePath}`, res);
+  }
 }
 
 async function handleRequest(req: IncomingMessage, res: ServerResponse) {
@@ -36,7 +116,11 @@ async function handleRequest(req: IncomingMessage, res: ServerResponse) {
   const currUrl = url.parse(req.url);
 
   if (currUrl.pathname) {
-    sendFile(currUrl.pathname, res);
+    if (currUrl.pathname.includes('api')) {
+      sendResource(currUrl.pathname, res);
+    } else {
+      sendFile(currUrl.pathname, res);
+    }
   } else {
     handleError('failed to parse url', res);
   }
--- src/public/scripts/index.ts
+++ src/public/scripts/index.ts
@@ -1,1 +1,77 @@
-$('h1')[0].innerHTML = 'hello';
+interface Countries {
+  [country: string]: string[];
+  continent: string[];
+  currency_code: string[];
+  city: string[];
+  tld: string[];
+  flag_base64: string[];
+}
+
+function createTable(headers: string[], rows: string[][]) {
+  const table = $('<table>', {
+    class: 'table overflow-scroll',
+    id: 'countryTable',
+  });
+  const header = $('<thead>');
+  const headerRow = header.append('<tr>');
+  headers.forEach((headerString) => {
+    const col = $('<th>', { scope: 'col' }).text(headerString);
+    headerRow.append(col);
+  });
+
+  table.append(headerRow);
+  const tableBody = $('<tbody>');
+
+  rows.forEach((row) => {
+    const currRow = $('<tr>');
+
+    row.forEach((cellData, index, arr) => {
+      let cell;
+      if (index === arr.length - 1) {
+        cell = $('<td>');
+        const flag = $('<img>', {
+          src: cellData,
+        });
+        flag.height('5rem');
+        flag.width('100%');
+        cell.append(flag);
+      } else {
+        cell = $('<td>').text(cellData);
+      }
+      currRow.append(cell);
+    });
+    tableBody.append(currRow);
+  });
+
+  table.append(tableBody);
+
+  $('#tableContainer').append(table);
+}
+
+function getCountriesData() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    const countriesData: Countries = JSON.parse(this.response);
+    if (countriesData) {
+      // // @ts-ignore
+      // delete countriesData.flag_base64;
+      const headers = Object.keys(countriesData).map((header) => header);
+      const rows = [];
+
+      for (let i = 0; i < countriesData.country.length; i += 1) {
+        const currRow = headers.map((header) => countriesData[header][i]);
+        rows.push(currRow);
+      }
+
+      createTable(headers, rows);
+    }
+  };
+
+  xhttp.open('GET', '/api/countries', true);
+  xhttp.send();
+}
+
+$('#dataButton').on('click', () => {
+  console.log('button clicked');
+  getCountriesData();
+});
