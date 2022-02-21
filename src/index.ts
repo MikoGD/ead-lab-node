526088882596d941f1a4bf4b4e3809f71006aa19
Mikael Escolin <mikaelescolin@gmail.com>
2022-02-21 12:00:04 +0000

    Add basic file handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
-import express from 'express';
+import http, { IncomingMessage, ServerResponse } from 'http';
+import fs from 'fs';
+import url from 'url';
+import path from 'path';
 
-const app = express();
-const PORT = 3000;
+function handleError(err: string, res: ServerResponse) {
+  res.write(err);
+  res.end();
+}
 
-app.get('/', (_, res) => {
-  res.send('Hello, World!');
-});
+async function sendFile(filePath: string, res: ServerResponse) {
+  fs.readFile(
+    path.join(
+      __dirname,
+      `public${filePath === '/' ? '/index.html' : filePath}`
+    ),
+    (err, data) => {
+      if (err) {
+        handleError(err.message, res);
+        return;
+      }
 
-app.listen(PORT, () => {
-  console.log('Server started');
-});
+      res.write(data);
+      res.end();
+    }
+  );
+}
+
+async function handleRequest(req: IncomingMessage, res: ServerResponse) {
+  if (!req.url) {
+    res.write('<h1>Error to read url</h1>');
+    res.end();
+    return;
+  }
+
+  const currUrl = url.parse(req.url);
+
+  if (currUrl.pathname) {
+    sendFile(currUrl.pathname, res);
+  } else {
+    handleError('failed to parse url', res);
+  }
+}
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    handleRequest(req, res);
+  })
+  .listen(3000);
