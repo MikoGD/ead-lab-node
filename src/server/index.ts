1394ea2217943cb1d3142e57d178c766436ac122
Mikael Escolin <mikaelescolin@gmail.com>
2022-03-07 15:20:14 +0000

    Refactor server
    - Extract functions from sendCountriesData

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,9 +3,13 @@ import fs from 'fs/promises';
 import url from 'url';
 import path from 'path';
 
+interface CountryEntry {
+  [key: string]: string;
+  country: string;
+}
 interface Country {
   [key: string]: string | undefined;
-  coutntry: string;
+  country?: string;
   continent?: string | undefined;
   currency_code?: string | undefined;
   city?: string | undefined;
@@ -13,7 +17,16 @@ interface Country {
   flag_base64?: string | undefined;
 }
 
-let countriesData: Record<string, Country> = {};
+const countryFiles = [
+  'name',
+  'continent',
+  'currency-code',
+  'capital-city',
+  'domain-tld',
+  'flag',
+];
+
+let mergedCountriesData: Record<string, Country> = {};
 
 function handleError(err: string, res: ServerResponse) {
   res.write(err);
@@ -47,61 +60,68 @@ async function sendFile(filePath: string, res: ServerResponse) {
   }
 }
 
+async function getCountryData() {
+  return await Promise.all(
+    /* Return an array of promises containing the promises of reading the files */
+    countryFiles.map((file) => {
+      const filePath = path.join(
+        __dirname,
+        `../country-objects/country-by-${file}.json`
+      );
+
+      try {
+        return fs.readFile(filePath);
+      } catch (err) {
+        throw new Error(
+          `failed to read file: ${filePath}${
+            typeof err === 'string' ? ` - ${err}` : ''
+          }`
+        );
+      }
+    })
+  );
+}
+
+/*
+A map is created where the key is the country and the value is an object with each data from the
+JSON files. This work as when it loops through the entries from the JSON files it gets the country
+key and spreads that object into the object contained in the key with the same country name.
+*/
+
+function mergeCountriesData(countriesData: CountryEntry[][]) {
+  const mergedCountriesData: Record<string, Country> = {};
+
+  countriesData.forEach((countryEntries) => {
+    countryEntries.forEach((entry) => {
+      const key = entry.country;
+
+      mergedCountriesData[key] = {
+        ...(mergedCountriesData[key] ?? {}),
+        ...entry,
+      };
+    });
+  });
+
+  return mergedCountriesData;
+}
+
 async function sendCountriesData(res: ServerResponse) {
-  if (Object.keys(countriesData).length > 0) {
+  if (Object.keys(mergedCountriesData).length > 0) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(countriesData));
+    res.write(JSON.stringify(mergedCountriesData));
     res.end();
   } else {
     const startTime = performance.now();
-    const names = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-name.json')
-    );
 
-    const currencies = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-currency-code.json')
-    );
-
-    const capitals = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-capital-city.json')
-    );
-
-    const domain = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-domain-tld.json')
-    );
-
-    const flag = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-flag.json')
-    );
-
-    const continents = fs.readFile(
-      path.join(__dirname, '../country-objects/country-by-continent.json')
-    );
-
-    const rawData = await Promise.all([
-      names,
-      continents,
-      currencies,
-      capitals,
-      domain,
-      flag,
-    ]);
+    const rawCountryData = await getCountryData();
 
     const readingTime = performance.now();
 
-    const parsedData: Record<string, any>[][] = rawData.map((currData) =>
+    const parsedData: CountryEntry[][] = rawCountryData.map((currData) =>
       JSON.parse(String(currData))
     );
 
-    const temp: Record<string, Country> = {};
-
-    parsedData.forEach((dataArr) => {
-      dataArr.forEach((elem) => {
-        temp[elem.country] = { ...(temp[elem.country] ?? {}), ...elem };
-      });
-    });
-
-    countriesData = temp;
+    const mergedCountriesData = mergeCountriesData(parsedData);
 
     const parsingTime = performance.now();
 
@@ -109,8 +129,9 @@ async function sendCountriesData(res: ServerResponse) {
     console.log(`Time to parse data: ${parsingTime - readingTime}`);
     console.log(`Total elapsed time: ${parsingTime - startTime}`);
 
+    console.log('sending files');
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(countriesData));
+    res.write(JSON.stringify(mergedCountriesData));
     res.end();
   }
 }
@@ -145,8 +166,4 @@ async function handleRequest(req: IncomingMessage, res: ServerResponse) {
   }
 }
 
-http
-  .createServer((req, res) => {
-    handleRequest(req, res);
-  })
-  .listen(3000);
+http.createServer(handleRequest).listen(3000);
